4fbe031abccb125a418acc2c93b273a2f44a0318
kkyyou <kkyyou2246@naver.com>
2020-11-20 16:42:36 +0900

    두번째 맵 추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+
+    public Camera mainCamera;
     
     private int score;
     public Text scoreText;
@@ -14,6 +16,9 @@ public class GameManager : MonoBehaviour
     public float hpDecreaseSpeed;
 
     private bool attackZombie = false;
+
+    public RightTargetZone rightTargetZone;
+    public LeftTargetZone leftTargetZone;
 
     private void Awake()
     {
@@ -31,7 +36,25 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int ran = Random.Range(0, 3);
+        if (ran == 1)
+        {
+            mainCamera.transform.position = new Vector3(0.5f, 0f, -10f);
+            Player.instance.transform.position = new Vector3(0.5f, -0.425f, 0f);
+        }
+        else
+        {
+            mainCamera.transform.position = new Vector3(0.5f, -14f, -10f);
+            Player.instance.transform.position = new Vector3(0.5f, -14.425f, 0f);
+
+            Vector3 rtp = rightTargetZone.transform.position;
+            rtp.y -= 14f;
+            rightTargetZone.transform.position = rtp;
+
+            Vector3 ltp = leftTargetZone.transform.position;
+            ltp.y -= 14f;
+            leftTargetZone.transform.position = ltp;
+        }
     }
 
     // Update is called once per frame
--- Assets/Scripts/ShakeCamera.cs
+++ Assets/Scripts/ShakeCamera.cs
@@ -14,7 +14,7 @@ public class ShakeCamera : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        initialPosition = new Vector3(0.5f, 0f, -10f);    
+        initialPosition = transform.position;  
     }
 
     // Update is called once per frame
